0f852518ff99eaaa39a83239c123dc56ca0d7d09
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-06-09 10:34:35 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,34 @@
+# 1: Variable practice problems
+# check the type of variable assigned using input function
+a = input("Enter a number value of a ")
+print(type(a))
+
+#  use camparison oper to find out wheter a given variable a is greater than b or not a = 34 b = 80
+a = 34
+b = 80
+print(a > b)
+
+# a = 34 b = 80
+a =int(input("enter number 1: "))
+b =int(input("enter number 2: "))
+print(" a is greater than b is ", a >b)
+
+# Create a variable name and store your name in it. Print it.
+#  create variable age and city and print
+a = input("Enter Your name:")
+print(("My name is:", a))
+b = input("Enter Your Age: ")
+print(("i am years old", b))
+c = input("Enter Your city:")
+print(("i live in:", c))
+
+# swap of two variables without using a third variable
+a = 5
+b = 4
+a, b = b, a
+print("a =", a)
+print("b =", b)
+
+
+print("The converted number from float to int:", g)
+print(type(g))
