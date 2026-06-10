3bb870696e598db048511cfb299b1a0e4322b827
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-06-10 11:41:28 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+# 2: Data Types problems
+# create variables of types
+
+a = 10          # Integer
+b = 10.5        # Float
+c = "Shahbaz"   # String
+d = False       # Boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))# type boolen
+
+# Conversion of types
+# integer to string
+de = str(input("Enter a str number:"))
+print("Converted integer:", de)
+print(type(de))
+
+# string to integer
+f = str(input("Enter a str number:"))
+f = int(f)
+print("Converted integer:", f)
+print(type(f))
+
+# Float to integer
+g = float(input("Enter a float number:"))
+g = int(g)
+print("The converted number from float to int:", g)
+print(type(g))
