fa96481ba42b6204aaf7146fd0702fc2c27b4de8
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-06-11 13:00:31 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,42 @@
+# Operators
+
+# Take two numbers and perform:
+# Addition operators
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum of two number:", a + b)
+print("Subtraction of two number:", a - b)
+print("Multipliction of two number:", a * b)
+print("Divison of two number:", a / b)
+
+# Find the remainder when 37 is divided by 5.
+a = 37
+b = 5
+print("the remainder is:", a%b)
+
+# wap pf python find an average of two numbers entered by user
+a =int(input("enter number 1: "))
+b =int(input("enter number 2: "))
+
+print("The average of these two number is ", (a + b)/2)
+
+# Calculate the square of a number entered by the user.
+c = int(input("enter a number:"))
+print("The square of the number is:", c**2)
+
+# Check whether a numinputber is even or odd using %.
+a = int(input("Enter a number: "))
+if a % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
+#  use camparison oper to find out wheter a given variable a is greater than b or not a = 34 b = 80
+a = 34
+b = 80
+print(a > b)
+
+# a = 34 b = 80
+a =int(input("enter number 1: "))
+b =int(input("enter number 2: "))
+print(" a is greater than b is ", a >b)
