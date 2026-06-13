4426b808b5e1bf03b3acb454d198aa80c03b7857
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-06-13 23:38:44 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,87 @@
+# write a program to store seven fruits in list enterd by user
+fruits  =[]
+f1 = (input("enter fruits  name: "))
+fruits. append (f1)
+f2 =(input("enter fruits name: "))
+fruits. append (f2)
+f3 = (input("Enter fruits name: "))
+fruits. append (f3)
+f4 = (input ("Enter fruits name: "))
+fruits. append (f4)
+f5 = (input("Enter fruits name: "))
+fruits. append (f5)
+f6 =(input("Enter fruits name: "))
+fruits. append (f6)
+f7 =(input("Enter fruits name: "))
+fruits. append (f7)
+
+fruits.sort()
+print(fruits)
+
+# write a program to acccept marks of 6 students and  display them in a sorted manner
+Marks  =[]
+f1 = int(input("enter Marks  name: "))
+Marks. append (f1)
+f2 =int(input("enter Marks name: "))
+Marks. append (f2)
+f3 = int(input("Enter Marks name: "))
+Marks. append (f3)
+f4 = int(input ("Enter Marks name: "))
+Marks. append (f4)
+f5 = int(input("Enter Marks name: "))
+Marks. append (f5)
+f6 =int(input("Enter Marks name: "))
+Marks. append (f6)
+
+Marks.sort()
+print(Marks)
+
+# write a program to sum a list with 4 numbers
+l =[3, 2, 4, 1]
+print(sum(l))
+
+# Create a list of 5 numbers and print it.
+a =[2, 1, 3, 5, 6]
+print(a)
+
+# print the first and last element of a list.
+b =[2, 1, 3, 5, 6]
+print(b[0], b[-1])
+
+# Add an element to a list.
+c =[2, 1, 3, 5, 6]
+c.append(45)
+print(c)
+
+# Remove an element from a list.
+l1 = [2, 1, 3, 5, 6]
+l1.remove(2)
+print(l1)
+
+# Find the length of a list.
+l1 = [2, 1, 3, 5, 6]
+print(len(l1))
+
+# Find the maximum and minimum number in a list.
+l2 = [2, 1, 3, 5, 45, 6]
+print(max(l2))
+print(min(l2))
+
+# Sort a list in ascending order.
+l3 = [2, 10, 5, 3, 45, 6]
+l3.sort()
+print(l3)
+
+# Reverse a list.
+m1 = [2, 10, 5, 3, 45, 6]
+m1.reverse()
+print(m1)
+
+# Count how many times an element appears in a list.
+m2 = [2, 10, 2, 3, 45, 6, 2]
+# m2.count(2)
+print(m2.count(2))
+
+# Find the sum of all elements in a list.
+m3 = [2, 3, 4, 1, 5]
+print(sum(m3))
