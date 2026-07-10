cbd785e03387ebf49a7af8d32bbedc9952393db8
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2026-07-10 15:05:06 +0530

    Create PrimeNumberFunction.py

---
+++ PrimeNumberFunction.py
@@ -0,0 +1,12 @@
+# Prime Number Function
+def prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+print(prime(13))
